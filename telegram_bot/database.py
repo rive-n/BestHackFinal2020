7a80007293a2ba7b21442e5dbf4a4b027b480570
R1V3N <45512613+PragmaEdragon@users.noreply.github.com>
2020-05-22 12:16:07 +0300

    Update database.py

--- telegram_bot/database.py
+++ telegram_bot/database.py
@@ -2,7 +2,7 @@ import psycopg2
 
 conn = psycopg2.connect(dbname="best_hack_2020_finals",
                         user = "postgres",
-                        password = '2254321R1V3N@',
+                        password = '',
                         host = "localhost",
                         port = '5432')
 
